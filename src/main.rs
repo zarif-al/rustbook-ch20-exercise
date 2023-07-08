322f681af28d724269d73b3b0b61c6c530881b3a
Abdullah Al Zarif <zarif_al96@outlook.com>
2023-07-08 16:21:01 +0600

    refactor: use thread pool

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,16 @@ use std::{
     time::Duration,
 };
 
+use rustbook_ch20_exercise::ThreadPool;
+
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        handle_connection(stream);
+        pool.execute(|| handle_connection(stream));
     }
 }
 
