284aadffcf625d35459abe61371435d7622f858a
Abdullah Al Zarif <zarif_al96@outlook.com>
2023-07-08 12:03:58 +0600

    refactor: simplify logic

--- src/main.rs
+++ src/main.rs
@@ -24,22 +24,17 @@ fn handle_connection(mut stream: TcpStream) {
     let request_route = request_line.next().unwrap();
 
     if request_method == "GET" {
-        if request_route == "/" {
-            let status_line = "HTTP/1.1 200 OK";
-            let contents = fs::read_to_string("hello.html").unwrap();
-            let length = contents.len();
-
-            let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
-            stream.write_all(response.as_bytes()).unwrap();
+        let (status_line, filename) = if request_route == "/" {
+            ("HTTP/1.1 200 OK", "hello.html")
         } else {
-            let status_line = "HTTP/1.1 404 NOT FOUND";
-            let contents = fs::read_to_string("404.html").unwrap();
-            let length = contents.len();
+            ("HTTP/1.1 404 NOT FOUND", "404.html")
+        };
 
-            let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+        let contents = fs::read_to_string(filename).unwrap();
+        let length = contents.len();
+        let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
 
-            stream.write_all(response.as_bytes()).unwrap();
-        }
+        stream.write_all(response.as_bytes()).unwrap();
     } else {
         println!("We have a request with another method")
     }
