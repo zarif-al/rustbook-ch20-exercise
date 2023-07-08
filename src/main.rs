83c96fb9e1cd4ec02f88eb2621ddd1bcc1067093
Abdullah Al Zarif <zarif_al96@outlook.com>
2023-07-08 17:45:28 +0600

    refactor: use 0.0.0.0 instead of 127.0.0.1

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,9 @@ use std::{
 use rustbook_ch20_exercise::ThreadPool;
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    println!("Starting multi-threaded server.");
+
+    let listener = TcpListener::bind("0.0.0.0:7878").unwrap();
     let pool = ThreadPool::new(4);
 
     for stream in listener.incoming().take(5) {
